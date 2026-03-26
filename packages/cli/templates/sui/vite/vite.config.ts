e6edc24739079c8616dfa8f0fa6b3ef12cc30697
Ezihe Godswill <ezihegodswill01@gmail.com>
2026-03-26 22:15:44 +0100

    feat: configure sui vite dependencies and tools

---
+++ packages/cli/templates/sui/vite/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      // @ts-ignore
+      babel: {
+        plugins: [['babel-plugin-react-compiler', { target: '19' }]],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
